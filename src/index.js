cbf235e348da5c0b970b7350913c421286a5c7e4
AlexeyGre4ko <alexeygre4ko@gmail.com>
2022-08-09 17:58:50 +0300

    fix: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  if(n<0)n*=-1
+  return (
+    parseFloat(
+        n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+      ) *Math.sign(n)
+  )
 }
